1e6680c3167eefb83bda836f9488df84a4bf7e90
Nichole <nmchristianson@gmail.com>
2021-04-14 16:43:27 -0700

    changes to js

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    });
-  };
-=======
-console.log('Hello Node!');
-conole.log(document)
->>>>>>> 5e1aa4726de9255dca038dd8d88bca2fd24944c8
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
